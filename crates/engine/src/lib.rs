ce67e352d6383565f418d84e605ff6535a0cdf37
Radu M <root@radu.sh>
2021-08-15 16:37:07 -0700

    Add a WasiExecutionContextBuilder
    Signed-off-by: Radu M <root@radu.sh>

--- crates/engine/src/lib.rs
+++ crates/engine/src/lib.rs
@@ -7,12 +7,12 @@ use wasi_experimental_http_wasmtime::HttpCtx;
 use wasi_nn_onnx_wasmtime::WasiNnTractCtx;
 use wasmtime::{Engine, Instance, InstancePre, Linker, Module, Store};
 
+/// Configuration for the engine.
 #[derive(Clone, Default)]
 pub struct Config {
     pub vars: Vec<(String, String)>,
     pub preopen_dirs: Vec<(String, String)>,
     pub allowed_http_hosts: Option<Vec<String>>,
-
     pub wasi_config: wasmtime::Config,
 }
 
@@ -35,15 +35,21 @@ impl Config {
     }
 }
 
+/// The execution context used by engines.
+/// This object can be directly created using its `new` functions,
+/// or granularly configured using a `WasiExecutionContextBuilder`.
 #[derive(Clone)]
 pub struct WasiExecutionContext<T: Default> {
     pub entrypoint_path: String,
     pub config: Config,
-
     pre: Arc<InstancePre<Context<T>>>,
     engine: Engine,
 }
 
+/// Runtime data for the instances.
+/// The generic type can either be directly the `-Data` type generated
+/// by witx-bindgen, or if additional host imports need to be configured,
+/// it can also contain state related to those imports.
 #[derive(Default)]
 pub struct Context<T> {
     pub wasi_ctx: Option<WasiCtx>,
@@ -51,7 +57,94 @@ pub struct Context<T> {
     pub runtime_data: Option<T>,
 }
 
+/// A builder that helps configure and build `WasiExecutionContext` instances.
+///
+/// Additional host imports can be defined using the linker and store fields.
+pub struct WasiExecutionContextBuilder<T: Default> {
+    pub config: Config,
+    pub engine: Engine,
+    pub store: Store<Context<T>>,
+    pub linker: Linker<Context<T>>,
+}
+
+impl<T: Default> WasiExecutionContextBuilder<T> {
+    /// Create a new `WasiExecutionContextBuilder`.
+    pub fn new(config: Config) -> Result<Self, Error> {
+        let engine = Engine::new(&config.wasi_config)?;
+        let linker: Linker<Context<T>> = Linker::new(&engine);
+        let store: Store<Context<T>> = Store::new(&engine, Context::default());
+
+        Ok(Self {
+            config,
+            engine,
+            store,
+            linker,
+        })
+    }
+
+    /// Configure support for the core WASI API.
+    pub fn add_wasi(&mut self) -> Result<&mut Self, Error> {
+        wasmtime_wasi::add_to_linker(&mut self.linker, |host| host.wasi_ctx.as_mut().unwrap())?;
+        Ok(self)
+    }
+
+    /// Configure support for experimental outbound HTTP support.
+    pub fn add_experimental_http(&mut self) -> Result<&mut Self, Error> {
+        HttpCtx::new(self.config.allowed_http_hosts.clone(), None)?
+            .add_to_generic_linker(&mut self.linker)?;
+        Ok(self)
+    }
+
+    /// Configure WASI NN for the current context using the self-contained
+    /// ONNX implementation for WASI NN.
+    pub fn add_nn(&mut self) -> Result<&mut Self, Error> {
+        wasi_nn_onnx_wasmtime::add_to_linker(&mut self.linker, |host| {
+            host.nn_ctx.as_mut().unwrap()
+        })?;
+        Ok(self)
+    }
+
+    /// Configure all available host imports.
+    ///
+    /// Currently, this includes core WASI, experimental HTTP
+    /// support, and the ONNX implementation of WASI NN.
+    pub fn add_all(&mut self) -> Result<&mut Self, Error> {
+        self.add_wasi()?;
+        self.add_experimental_http()?;
+        self.add_nn()?;
+
+        Ok(self)
+    }
+
+    /// Create a `WasiExecutionContext` using the configured store
+    /// and linker, and pre-instantiate the entrypoint WebAssembly module.
+    pub fn build(&mut self, entrypoint_path: String) -> Result<WasiExecutionContext<T>, Error> {
+        let start = Instant::now();
+
+        let (config, engine) = (self.config.clone(), self.engine.clone());
+        let module = Module::from_file(&self.engine, entrypoint_path.clone())?;
+        let pre = Arc::new(self.linker.instantiate_pre(&mut self.store, &module)?);
+
+        log::info!(
+            "Created engine from WASI component in: {:?}",
+            start.elapsed()
+        );
+
+        Ok(WasiExecutionContext {
+            entrypoint_path,
+            config,
+            pre,
+            engine,
+        })
+    }
+}
+
 impl<T: Default> WasiExecutionContext<T> {
+    /// Creates a new `WasiExecutionContext` based on the WASI component reference
+    /// from the registry and configuration.
+    ///
+    /// The WASI context created in this way enables support for HTTP and WASI NN.
+    /// This can be configured by using a `WasiExecutionContextBuilder`.
     pub async fn new(
         server: &str,
         reference: &str,
@@ -63,41 +156,16 @@ impl<T: Default> WasiExecutionContext<T> {
         Self::preinstantiate(entrypoint_path, config)
     }
 
+    /// Create a new `WasiExecutionContext` from a local file.
+    /// See `WasiExecutionContext::new` for details about the resulting instance.
     pub fn new_from_local(entrypoint_path: String, config: Config) -> Result<Self, Error> {
         Self::preinstantiate(entrypoint_path, config)
     }
 
     fn preinstantiate(entrypoint_path: String, config: Config) -> Result<Self, Error> {
-        let start = Instant::now();
-
-        let engine = Engine::new(&config.wasi_config)?;
-        let mut linker: Linker<Context<T>> = Linker::new(&engine);
-        let mut store: Store<Context<T>> = Store::new(&engine, Context::default());
-
-        Self::link(&mut linker, &config)?;
-        let module = Module::from_file(linker.engine(), entrypoint_path.clone())?;
-        let pre = Arc::new(linker.instantiate_pre(&mut store, &module)?);
-
-        log::info!(
-            "Created engine from WASI component in: {:?}",
-            start.elapsed()
-        );
-
-        Ok(Self {
-            entrypoint_path,
-            config,
-            pre,
-            engine,
-        })
-    }
-
-    fn link(mut linker: &mut Linker<Context<T>>, config: &Config) -> Result<(), Error> {
-        wasmtime_wasi::add_to_linker(linker, |host| host.wasi_ctx.as_mut().unwrap())?;
-        let http = HttpCtx::new(config.allowed_http_hosts.clone(), None)?;
-        http.add_to_generic_linker(&mut linker)?;
-        wasi_nn_onnx_wasmtime::add_to_linker(linker, |host| host.nn_ctx.as_mut().unwrap())?;
-
-        Ok(())
+        let mut builder = WasiExecutionContextBuilder::new(config)?;
+        builder.add_all()?;
+        builder.build(entrypoint_path)
     }
 
     fn create_store(&self, data: Option<T>) -> Result<Store<Context<T>>, Error> {
@@ -135,6 +203,8 @@ impl<T: Default> WasiExecutionContext<T> {
         Ok(preopen_dirs)
     }
 
+    /// Prepare the execution by finishing the instantiation proces for the module
+    /// using real runtime data, then return the store and instance to be used by the engine.
     pub fn prepare_exec(&self, data: Option<T>) -> Result<(Store<Context<T>>, Instance), Error> {
         let mut store = self.create_store(data)?;
         let instance = self.pre.instantiate(&mut store)?;
